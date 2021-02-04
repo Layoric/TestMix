b6f5b5d9cea937a73af6f073a58d0d98a1c65d9c
Darren Reid <darren@reidmail.org>
2021-02-04 11:36:43 +1100

    Initial commit

---
+++ TestMix.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+using System;
+using ServiceStack;
+using TestMix.ServiceModel;
+
+namespace TestMix.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
